0ea2bf86a29801500f3d8efa8ddceb366e76c909
FarisArch <farisaimansaser@gmail.com>
2020-11-22 22:00:52 +0800

    Initial commit

--- PyAutoGui-MP3.py
+++ PyAutoGui-MP3.py
@@ -2,23 +2,26 @@ import webbrowser
 import time
 import pyautogui
 import sys
+condition = True
 webbrowser.open("https://www.mp3juices.cc/")
 time.sleep(3)
-search =pyautogui.prompt("What to search?:")
-if search == None:
-    sys.exit()
-else:
-    pyautogui.click(671, 376)
-    pyautogui.typewrite(search)
-    pyautogui.click(951, 374)
-    time.sleep(5)
-    pyautogui.click(556,161)
-    time.sleep(5)
-    pyautogui.click(654,428)
-    time.sleep(5)
-    pyautogui.click(583,557)
-    time.sleep(5)
-    pyautogui.click(475,427)
-    time.sleep(3)
-    pyautogui.click(879,525)
-    sys.exit()
+while condition:
+    search =pyautogui.prompt("What to search?:")
+    if search == None:
+        sys.exit()
+        condition = False
+    else:
+        pyautogui.click(671, 376)
+        pyautogui.typewrite(search)
+        pyautogui.click(951, 374)
+        time.sleep(5)
+        pyautogui.click(556,161)
+        time.sleep(5)
+        pyautogui.click(654,428)
+        time.sleep(5)
+        pyautogui.click(583,557)
+        time.sleep(5)
+        pyautogui.click(475,427)
+        time.sleep(3)
+        pyautogui.click(879,525)
+        sys.exit()
